50d40421f4f7d5000bf56ff0175d7182e14d83a2
bchien21 <brianchien04172@gmail.com>
2025-07-10 12:02:53 -0400

    Camera Added

--- source/ace_isaac/ace_isaac/tasks/direct/jetbot_camera/jetbot_camera_env_cfg.py
+++ source/ace_isaac/ace_isaac/tasks/direct/jetbot_camera/jetbot_camera_env_cfg.py
@@ -1,44 +1,51 @@
 from ace_isaac.robots.jetbot import JETBOT_CONFIG
 
 import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg
 from isaaclab.envs import DirectRLEnvCfg
 from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.sensors import CameraCfg
 from isaaclab.sim import SimulationCfg
 from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
 
 
-# Extra Imports Needed for Camera
-#from isaaclab.sensors import TiledCamera, TiledCameraCfg, save_images_to_file
+
+@configclass
+class JetbotSceneCfg(InteractiveSceneCfg):
+    room_cfg = AssetBaseCfg(prim_path="{ENV_REGEX_NS}/room", spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Environments/Simple_Room/simple_room.usd"))
+    
+    jetbot: ArticulationCfg = JETBOT_CONFIG.replace(prim_path="{ENV_REGEX_NS}/Robot", init_state=ArticulationCfg.InitialStateCfg(pos=(-.6,0,0)))
+
+    camera = CameraCfg(
+        data_types=["rgb"],
+        prim_path="{ENV_REGEX_NS}/Robot/chassis/rgb_camera/jetbot_camera",
+        spawn=None,
+        height=224,
+        width=224,
+        update_period=.1
+    )
+
+    goal_marker = RigidObjectCfg(prim_path="{ENV_REGEX_NS}/marker", 
+                                 spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/red_block.usd"), 
+                                 init_state=RigidObjectCfg.InitialStateCfg(pos=(.3,0,0)))
+
 
 @configclass
 class JetbotCameraEnvCfg(DirectRLEnvCfg):
     # env
     decimation = 2
-    episode_length_s = 7.0
+    episode_length_s = 5.0
     # - spaces definition
     action_space = 2
-    observation_space = 7
+    action_scale = 100.0
     state_space = 0
-    # simulation
-    sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
-    # robot(s)
-    robot_cfg: ArticulationCfg = JETBOT_CONFIG.replace(prim_path="/World/envs/env_.*/Robot")
-
-    # # camera
-    # tiled_camera: TiledCameraCfg = TiledCameraCfg(
-    #     prim_path="/World/envs/env_.*/Camera",
-    #     offset=TiledCameraCfg.OffsetCfg(pos=(-5.0, 0.0, 2.0), rot=(1.0, 0.0, 0.0, 0.0), convention="world"),
-    #     data_types=["rgb"],
-    #     spawn=sim_utils.PinholeCameraCfg(
-    #         focal_length=24.0, focus_distance=400.0, horizontal_aperture=20.955, clipping_range=(0.1, 20.0)
-    #     ),
-    #     width=100,
-    #     height=100,
-    # )
-    # write_image_to_file = False
-    # viewer = ViewerCfg(eye=(20.0, 20.0, 20.0))
 
     # scene
-    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=40, env_spacing=4.0, replicate_physics=True)
+    scene: InteractiveSceneCfg = JetbotSceneCfg(num_envs=1, env_spacing=15.0, replicate_physics=True)
     dof_names = ["left_wheel_joint", "right_wheel_joint"]
+
+    observation_space = [scene.camera.height, scene.camera.width, 3]
+
+    # simulation
+    sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
