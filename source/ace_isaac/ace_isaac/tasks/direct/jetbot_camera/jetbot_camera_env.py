3c67bc63fa73f602533e6f70738d4028a5223648
bchien21 <brianchien04172@gmail.com>
2025-07-13 18:26:20 -0400

    Further changes to Jetbot Camera Env

--- source/ace_isaac/ace_isaac/tasks/direct/jetbot_camera/jetbot_camera_env.py
+++ source/ace_isaac/ace_isaac/tasks/direct/jetbot_camera/jetbot_camera_env.py
@@ -8,16 +8,12 @@ from __future__ import annotations
 import math
 import torch
 from collections.abc import Sequence
-import gymnasium as gym
-import numpy as np
 
 import isaaclab.sim as sim_utils
-#from isaaclab.assets import Articulation, ArticulationCfg
+from isaaclab.assets import Articulation, RigidObject
 from isaaclab.envs import DirectRLEnv
 from isaaclab.sim.spawners.from_files import GroundPlaneCfg, spawn_ground_plane
-import isaaclab.utils.math as math_utils
-from .jetbot_camera_env_cfg import JetbotCameraEnvCfg
-
+from isaaclab.sensors import TiledCamera
 
 from isaaclab.markers import VisualizationMarkers, VisualizationMarkersCfg
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
@@ -43,24 +39,19 @@ def define_markers() -> VisualizationMarkers:
     return VisualizationMarkers(cfg=marker_cfg)
 
 
+from .jetbot_camera_env_cfg import JetbotCameraEnvCfg
+
 class JetbotCameraEnv(DirectRLEnv):
     cfg: JetbotCameraEnvCfg
 
     def __init__(self, cfg: JetbotCameraEnvCfg, render_mode: str | None = None, **kwargs):
         super().__init__(cfg, render_mode, **kwargs)
-
-        self.action_scale = self.cfg.action_scale
-
         self.dof_idx, _ = self.robot.find_joints(self.cfg.dof_names)
-        print(self.dof_idx)
-
-        #self.prev_dist = torch.linalg.norm(self.robot.data.root_pos_w - self.goal_marker.data.root_pos_w, dim=-1)
 
     def _setup_scene(self):
-
-        self.robot        = self.scene["jetbot"]
-        self.robot_camera = self.scene["camera"]
-        self.goal_marker  = self.scene["goal_marker"]
+        self.robot = Articulation(self.cfg.robot_cfg)
+        self.goal_marker = RigidObject(self.cfg.goal_cfg)
+        self.robot_camera = TiledCamera(self.cfg.tiled_camera)
 
         # add ground plane
         spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg())
@@ -76,11 +67,10 @@ class JetbotCameraEnv(DirectRLEnv):
 
         self.up_dir = torch.tensor([0.0, 0.0, 1.0]).cuda()  
         self.yaws = torch.zeros((self.cfg.scene.num_envs, 1)).cuda()
-
         self.commands = torch.randn((self.cfg.scene.num_envs, 3)).cuda()
         self.commands[:,-1] = 0.0
         self.commands = self.commands/torch.linalg.norm(self.commands, dim=1, keepdim=True)
-
+        
         # offsets to account for atan range and keep things on [-pi, pi]
         ratio = self.commands[:,1]/(self.commands[:,0]+1E-8)
         gzero = torch.where(self.commands > 0, True, False)
@@ -95,21 +85,17 @@ class JetbotCameraEnv(DirectRLEnv):
         self.marker_offset[:,-1] = 0.5
         self.forward_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
         self.command_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
-
-        # self.forwards = math_utils.quat_apply(self.robot.data.root_link_quat_w, self.robot.data.FORWARD_VEC_B)
-        # root_pos = self.robot.data.root_pos_w                # (N,3)
-        # goal_vec = self.goal_marker.data.root_pos_w - root_pos          # (N,3)
-        # goal_vec = goal_vec/torch.linalg.norm(goal_vec, dim=-1, keepdim=True)
-
+        
 
     def _visualize_markers(self):
 
         root_pos = self.robot.data.root_pos_w                # (N,3)
         goal_vec = self.goal_marker.data.root_pos_w - root_pos          # (N,3)
+
+        #print(f"Position of the Red Block: {self.goal_marker.data.root_pos_w }")
+
         goal_vec = goal_vec/torch.linalg.norm(goal_vec, dim=-1, keepdim=True)
         self.commands = goal_vec
-
-        # offsets to account for atan range and keep things on [-pi, pi]
         ratio = self.commands[:,1]/(self.commands[:,0]+1E-8)
         gzero = torch.where(self.commands > 0, True, False)
         lzero = torch.where(self.commands < 0, True, False)
@@ -132,7 +118,7 @@ class JetbotCameraEnv(DirectRLEnv):
         self.visualization_markers.visualize(loc, rots, marker_indices=indices)
 
     def _pre_physics_step(self, actions: torch.Tensor) -> None:
-        self.actions = self.action_scale*actions.clone()
+        self.actions = 25*actions.clone()# + torch.ones_like(actions)
         self._visualize_markers()
 
     def _apply_action(self) -> None:
@@ -147,39 +133,27 @@ class JetbotCameraEnv(DirectRLEnv):
 
         return {"policy": camera_data.clone()}
 
-    def _get_rewards(self):
-
-        # current root world pos & goal
-        self.forwards = math_utils.quat_apply(self.robot.data.root_link_quat_w, self.robot.data.FORWARD_VEC_B)
-
-        root_pos = self.robot.data.root_pos_w                # (N,3)
-        goal_vec = self.goal_marker.data.root_pos_w - root_pos          # (N,3)
-        goal_vec = goal_vec/torch.linalg.norm(goal_vec, dim=-1, keepdim=True)
-
+    def _get_rewards(self) -> torch.Tensor:
         forward_reward = self.robot.data.root_com_lin_vel_b[:,0].reshape(-1,1)
-        alignment_reward = torch.sum(self.forwards * goal_vec, dim=-1, keepdim=True)
 
-        total_reward = forward_reward*torch.exp(alignment_reward)
-
+        forwards = math_utils.quat_apply(self.robot.data.root_link_quat_w, self.robot.data.FORWARD_VEC_B)
+        alignment_reward = torch.sum(forwards * self.commands, dim=-1, keepdim=True)
+        
+        # total_reward = forward_reward*alignment_reward
+        # total_reward = forward_reward*alignment_reward + forward_reward
+        # total_reward = forward_reward*torch.exp(alignment_reward)
 
         # current root world pos & goal
         root_pos = self.robot.data.root_pos_w                # (N,3)
         goal_vec = self.goal_marker.data.root_pos_w - root_pos          # (N,3)
 
-        # distance to goal
+        # arrival bonus
         dist = torch.linalg.norm(goal_vec, dim=-1, keepdim=True)  # (N,1)
         arrived = (dist < 0.2).to(torch.float32) * 2.0
 
-        total_reward += arrived
-        total_reward = total_reward.flatten()
-
-
-        if (self.common_step_counter % 10 == 0):
-            #print(f"Velocity Contribution is {progress}")
-            print(f"Reward at step {self.common_step_counter} is {total_reward} with shape {total_reward.shape}")
+        total_reward = forward_reward + alignment_reward + arrived
 
         return total_reward
-
 
     def _get_dones(self) -> tuple[torch.Tensor, torch.Tensor]:
         time_out = self.episode_length_buf >= self.max_episode_length - 1
@@ -191,6 +165,15 @@ class JetbotCameraEnv(DirectRLEnv):
             env_ids = self.robot._ALL_INDICES
         super()._reset_idx(env_ids)
 
+        default_root_state = self.robot.data.default_root_state[env_ids]
+        default_root_state[:, :3] += self.scene.env_origins[env_ids]
+
+        self.robot.write_root_state_to_sim(default_root_state, env_ids)
+
+        default_block_root_state = self.goal_marker.data.default_root_state[env_ids]
+        default_block_root_state[:, :3] += self.scene.env_origins[env_ids]
+
+        self.goal_marker.write_root_state_to_sim(default_block_root_state, env_ids)
 
         root_pos = self.robot.data.root_pos_w                # (N,3)
         goal_vec = self.goal_marker.data.root_pos_w - root_pos          # (N,3)
@@ -205,16 +188,4 @@ class JetbotCameraEnv(DirectRLEnv):
         offsets = torch.pi*plus - torch.pi*minus
         self.yaws[env_ids] = torch.atan(ratio).reshape(-1,1) + offsets.reshape(-1,1)
 
-
-        default_goal_root_state = self.goal_marker.data.default_root_state[env_ids]
-        default_goal_root_state[:, :3] += self.scene.env_origins[env_ids]
-        self.goal_marker.write_root_state_to_sim(default_goal_root_state, env_ids)
-
-        # set the root state for the reset envs
-        default_root_state = self.robot.data.default_root_state[env_ids]
-        default_root_state[:, :3] += self.scene.env_origins[env_ids]
-        self.robot.write_root_state_to_sim(default_root_state, env_ids)
-
         self._visualize_markers()
-
-        #self.prev_dist = torch.linalg.norm(self.robot.data.root_pos_w - self.goal_marker.data.root_pos_w, dim=-1)
--- source/ace_isaac/ace_isaac/tasks/direct/jetbot_camera/jetbot_camera_env_cfg.py
+++ source/ace_isaac/ace_isaac/tasks/direct/jetbot_camera/jetbot_camera_env_cfg.py
@@ -1,35 +1,19 @@
-from ace_isaac.robots.jetbot import JETBOT_CONFIG
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
 
-import isaaclab.sim as sim_utils
-from isaaclab.assets import ArticulationCfg, AssetBaseCfg, RigidObjectCfg
+from isaac_lab_tutorial.robots.jetbot import JETBOT_CONFIG
+
+from isaaclab.assets import ArticulationCfg, RigidObjectCfg
 from isaaclab.envs import DirectRLEnvCfg
 from isaaclab.scene import InteractiveSceneCfg
-from isaaclab.sensors import CameraCfg, TiledCameraCfg
 from isaaclab.sim import SimulationCfg
 from isaaclab.utils import configclass
+
+from isaaclab.sensors import TiledCameraCfg
+import isaaclab.sim as sim_utils
 from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
-
-
-
-@configclass
-class JetbotSceneCfg(InteractiveSceneCfg):
-
-    #room_cfg = AssetBaseCfg(prim_path="{ENV_REGEX_NS}/room", spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Environments/Simple_Room/simple_room.usd"))
-    
-    jetbot: ArticulationCfg = JETBOT_CONFIG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-
-    camera = CameraCfg(
-        data_types=["rgb"],
-        prim_path="{ENV_REGEX_NS}/Robot/chassis/rgb_camera/jetbot_camera",
-        spawn=None,
-        height=224,
-        width=224,
-        update_period=.1
-    )
-
-    goal_marker = RigidObjectCfg(prim_path="{ENV_REGEX_NS}/marker", 
-                                 spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/red_block.usd"), 
-                                 init_state=RigidObjectCfg.InitialStateCfg(pos=(.75,0.4,0)))
 
 
 @configclass
@@ -37,22 +21,30 @@ class JetbotCameraEnvCfg(DirectRLEnvCfg):
     # env
     decimation = 2
     episode_length_s = 5.0
-
-    action_scale = 100.0
-    state_space = 0
-
-    # scene
-    scene: InteractiveSceneCfg = JetbotSceneCfg(num_envs=15, env_spacing=20.0, replicate_physics=True)
-    dof_names = ["left_wheel_joint", "right_wheel_joint"]
-
-    num_channels = 3
-    # num_observations = num_channels * scene.camera.height * scene.camera.width
-
+    # - spaces definition
     action_space = 2
+    # observation_space = 9
+    #observation_space = 3
     state_space = 0
-    observation_space = [scene.camera.height, scene.camera.width, 3]
-    #num_channels * scene.camera.height * scene.camera.width
-    # [tiled_camera.height, tiled_camera.width, 3]
-
     # simulation
     sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
+    # robot(s)
+    robot_cfg: ArticulationCfg = JETBOT_CONFIG.replace(prim_path="/World/envs/env_.*/Robot")
+
+    # camera
+    tiled_camera: TiledCameraCfg = TiledCameraCfg(
+        prim_path="/World/envs/env_.*/Robot/chassis/rgb_camera/jetbot_camera",
+        offset=TiledCameraCfg.OffsetCfg(pos=(-5.0, 0.0, 2.0), rot=(1.0, 0.0, 0.0, 0.0), convention="world"),
+        data_types=["rgb"],
+        spawn=None,
+        width=224,
+        height=224,
+    )
+
+    goal_cfg = RigidObjectCfg(prim_path="/World/envs/env_.*/marker", spawn=sim_utils.UsdFileCfg(usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Blocks/red_block.usd"), init_state=RigidObjectCfg.InitialStateCfg(pos=(.6,.3,0)))
+
+    observation_space = [tiled_camera.height, tiled_camera.width, 3]
+
+    # scene
+    scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=15, env_spacing=20.0, replicate_physics=True)
+    dof_names = ["left_wheel_joint", "right_wheel_joint"]
